fe6fafed203949618e5aec376eb5ecdd0e97789c
AAWA-byt <mail@aaronwagner.earth>
2022-12-13 11:21:56 +0100

    :sparkles: added buttons and footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,31 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>lolo.school</h1>
+        <input type="submit" value="Dokumentation" />
+        <input type="submit" value="Git-Server" />
       </header>
+
+      <div class="footer-dark">
+    <footer>
+        <center>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Quick Links</h3>
+                        <ul>
+                            <li><a href="https://www.lore-lorentz-schule.de/impressum">Impressum</a></li>
+                            <li><a href="https://www.lore-lorentz-schule.de/datenschutz">Datenschutz</a></li>
+                            <li><a href="https://github.com/Lore-Lorentz-Schule/landing-page-lolo.git">Source Code</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <p class="copyright">Developed by Aaron Wagner</p>
+                <p class="copyright">© 2022  </p>
+            </div>
+        </center>
+    </footer>
+</div>
     </div>
   );
 }
